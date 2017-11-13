6da2e8e119c131155dc7421d2e3e6b82ca8094f9
WiraXie <wiraxie.wira_xie@ymail.com>
2017-11-13 11:22:09 +0700

    2nd

--- src/app/employees/shared/employee.model.ts
+++ src/app/employees/shared/employee.model.ts
@@ -1,5 +1,6 @@
-export class Employee {
-    $key : string; //database primary key
+export class Employee 
+{
+    $key : number; //database primary key
     name : string;
     position : string;
     office : string;
